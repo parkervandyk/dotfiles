231fd5321b620614c2bcdf5095950bbc18d34e62
parkervandyk <parker@parkervandyk.com>
2024-05-31 17:37:46 -0500

    updates

---
+++ .config/nvim/lua/parker/plugins/vimwiki.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "vimwiki/vimwiki",
+    init = function()
+      vim.g.vimwiki_list = {
+        {
+          path = "~/docs/vimwiki",
+          syntax = "default",
+          ext = ".wiki",
+        },
+      }
+    end,
+  },
+}
