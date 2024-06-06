64956210f77a14a7b32a4618593fa59cb475824c
Parker Van Dyk <parker@parkervandyk.com>
2024-06-06 12:38:26 -0500

    updating using gg

--- .config/nvim/lua/parker/plugins/init.lua
+++ .config/nvim/lua/parker/plugins/init.lua
@@ -2,9 +2,9 @@ return {
   "nvim-lua/plenary.nvim", -- lua functions that many plugins use
   "christoomey/vim-tmux-navigator", -- tmux & split window navigation
   "MunifTanjim/nui.nvim",
-  "rcarriga/nvim-notify",
-  "folke/noice.nvim",
   "michal-h21/vimwiki-sync",
   "iamcco/markdown-preview.nvim",
   "github/copilot.vim",
+  "pmizio/typescript-tools.nvim",
+  "folke/trouble.nvim",
 }
