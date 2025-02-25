5f7e772358ca383dc7db0dba3c0dcca95de8b2cf
Parker Van Dyk <parker@parkervandyk.com>
2025-02-25 13:25:30 -0600

    Delete .config/nvim directory

--- .config/nvim/lua/parker/plugins/indent-blankline.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "lukas-reineke/indent-blankline.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  main = "ibl",
-  opts = {
-    indent = { char = "┋" },
-    scope = { show_start = true, show_end = true },
-  },
-}
