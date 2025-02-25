ac3067d60ec4cdb609344f84c8e4341ff205b42a
Parker Van Dyk <parker@parkervandyk.com>
2025-02-25 13:56:33 -0600

    some more lazy updated

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,15 +1,12 @@
--- treesitter.lua
-require("lazy").setup({{
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function () 
-    local configs = require("nvim-treesitter.configs")
-
-    configs.setup({
-        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
-        sync_install = false,
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = "all",
         highlight = { enable = true },
-        indent = { enable = true },  
       })
-  end
-}})
+    end,
+  },
+}
